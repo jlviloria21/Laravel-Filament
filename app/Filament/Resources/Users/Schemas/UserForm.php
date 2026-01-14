8dfa5d4c499f138e5aaa7c0edd12cb25a39d16e8
Jaime Viloria <jlviloria21@gmail.com>
2026-01-14 06:18:21 -0500

    Segundo Commit

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -4,8 +4,13 @@ namespace App\Filament\Resources\Users\Schemas;
 
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
 use Filament\Schemas\Schema;
 use Filament\Schemas\Components\Section;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Illuminate\Support\Collection;
+use App\Models\State;
 
 class UserForm
 {
@@ -26,6 +31,28 @@ class UserForm
                         ->password()
                         ->required(),
                 ])->columns(4)
+                ->columnSpan('full'),
+
+                Section::make('Address Info')
+                ->schema([
+                    Select::make('country_id')
+                        ->relationship('country',titleAttribute:'name')
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->afterStateUpdated(fn (Set $set) => $set('state_id', null)) // limpia el segundo select
+                        ->required(),
+
+                        Select::make('state_id')
+                            ->options(fn (Get $get): Collection => $get('country_id')
+                            ? State::where('country_id', $get('country_id'))
+                                ->pluck('name', 'id')
+                            : collect())
+                            ->searchable()
+                            ->preload()
+                            ->live()
+                            ->required(),
+                ])->columns(3)
                 ->columnSpan('full')
             ]);
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
 }
